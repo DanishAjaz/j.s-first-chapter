49c3327a7f2bd61db97706d9dbc92116bbdb103a
DanishAjaz <156819621+DanishAjaz@users.noreply.github.com>
2024-03-03 17:59:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+//Q1&2 //alert ('Welcome to JavaScript');
+//Q3//alert ('welcome to my website \n happy coding')
+//Q4swal({text: "its me dani!", });
+//console.log( "hello i can run js through my web browser's consol")
